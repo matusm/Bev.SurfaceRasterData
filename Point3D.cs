e7932bc787b4da65fdfeab0713ea588968b3ea5e
Michael Matus <michael@matus.at>
2020-04-25 15:54:19 +0200

    Initial check-in of module Bev.SurfaceRasterData

---
+++ Point3D.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bev.SurfaceRasterData
+{
+    public class Point3D : IComparable
+    {
+        public Point3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Point3D objc = obj as Point3D;
+            if (X < objc.X) return -1;
+            if (X > objc.X) return 1;
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            return $"[Point3D - X:{X.ToString()} Y:{Y.ToString()} Z:{Z.ToString()}]";
+        }
+    }
+}
---
+++ SurfaceData.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bev.SurfaceRasterData
+{
+    public class SurfaceData
+    {
+
+        private readonly double[,] zValues;
+        private int runningIndex;
+        private const double invalidValue = double.NaN;
+
+        #region Ctor
+        public SurfaceData(int numPoints, int numProfiles)
+        {
+            NumberOfPointsPerProfile = numPoints;
+            NumberOfProfiles = numProfiles;
+            zValues = new double[NumberOfPointsPerProfile, NumberOfProfiles];
+            SetPropertiesToDefault();
+            ResetRunningIndex();
+        }
+        #endregion
+
+        #region Properties
+        public int NumberOfPointsPerProfile { get; private set; } // NumPoints
+        public int NumberOfProfiles { get; private set; } // NumProfiles
+        public double XScale { get; set; }
+        public double YScale { get; set; }
+        // following properties as proposed by Xiangqi Lan (2014)
+        public double ZScale { get; set; }
+        public string XUnit { get; set; }
+        public string YUnit { get; set; }
+        public string ZUnit { get; set; }
+        public double XOffset { get; set; }
+        public double YOffset { get; set; }
+        public double ZOffset { get; set; }
+        public bool IsMetricUnit { get; set; }
+        public string Description { get; set; }
+        public string Manufacturer { get; set; }
+        public string VersionField { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
+        public Dictionary<string, string> MetaData { get; set; }
+        #endregion
+
+        #region Methods
+
+        // the hight data is filled up point by point in the raster data array
+        // this is a slow process but compatible with usual file parsing techniques
+        public void FillUpData(double value)
+        {
+            int pointsIndex = runningIndex % NumberOfPointsPerProfile;
+            if (pointsIndex >= NumberOfPointsPerProfile)
+                return;
+            int profileIndex = runningIndex / NumberOfPointsPerProfile;
+            if (profileIndex >= NumberOfProfiles)
+                return;
+            zValues[pointsIndex, profileIndex] = value;
+            runningIndex++;
+        }
+
+        public double[] GetProfileFor(int profileIndex)
+        {
+            double[] profile = new double[NumberOfPointsPerProfile];
+            for (int i = 0; i < NumberOfPointsPerProfile; i++)
+            {
+                // this assures that NaN array is returned for a profile index outside the range
+                profile[i] = GetValueFor(i, profileIndex);
+            }
+            return profile;
+        }
+
+        public Point3D[] GetPointsProfileFor(int profileIndex)
+        {
+            Point3D[] profile = new Point3D[NumberOfPointsPerProfile];
+            for (int i = 0; i < NumberOfPointsPerProfile; i++)
+            {
+                // this assures that NaN array is returned for a profile index outside the range
+                profile[i] = GetPointFor(i, profileIndex);
+            }
+            return profile;
+        }
+
+        public double GetValueFor(int pointIndex, int profileIndex)
+        {
+            if (pointIndex < 0) return invalidValue;
+            if (profileIndex < 0) return invalidValue;
+            if (pointIndex >= NumberOfPointsPerProfile) return invalidValue;
+            if (profileIndex >= NumberOfProfiles) return invalidValue;
+            return ZOffset + zValues[pointIndex, profileIndex];
+        }
+
+        public Point3D GetPointFor(int pointIndex, int profileIndex)
+        {
+            double xCoordinate = XOffset + pointIndex * XScale;
+            double yCoordinate = YOffset + profileIndex * YScale;
+            if (pointIndex < 0) xCoordinate = invalidValue;
+            if (profileIndex < 0) yCoordinate = invalidValue;
+            if (pointIndex >= NumberOfPointsPerProfile) xCoordinate = invalidValue;
+            if (profileIndex >= NumberOfProfiles) yCoordinate = invalidValue;
+            return new Point3D(xCoordinate, yCoordinate, GetValueFor(pointIndex, profileIndex));
+        }
+
+        #endregion
+
+        #region Private stuff
+
+        private void ResetRunningIndex()
+        {
+            runningIndex = 0;
+        }
+
+        private void SetPropertiesToDefault()
+        {
+            XScale = 1.0;
+            YScale = 1.0;
+            ZScale = 1.0;
+            XUnit = "m";
+            YUnit = "m";
+            ZUnit = "m";
+            XOffset = 0.0;
+            YOffset = 0.0;
+            ZOffset = 0.0;
+            IsMetricUnit = true;
+            CreateDate = null;
+            ModifyDate = null;
+        }
+
+        #endregion
+    }
+}
