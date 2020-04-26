e0bde133d3a79d746e50f7db689312973c1f7554
Michael Matus <michael@matus.at>
2020-04-26 12:35:48 +0200

    Property renamed to IsDataComplete

--- SurfaceData.cs
+++ SurfaceData.cs
@@ -22,7 +22,7 @@ namespace Bev.SurfaceRasterData
         #endregion
 
         #region Properties
-        public bool IsDataFilled => (runningIndex >= NumberOfPointsPerProfile * NumberOfProfiles);
+        public bool IsDataComplete => (runningIndex >= NumberOfPointsPerProfile * NumberOfProfiles);
         public int NumberOfPointsPerProfile { get; private set; } // NumPoints
         public int NumberOfProfiles { get; private set; } // NumProfiles
         public double XScale { get; set; }
