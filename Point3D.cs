ed48476c509478683fa202d4b1081a6eea14cfcb
Michael Matus <michael@matus.at>
2025-05-05 21:07:53 +0200

    Create a type-specific comparison for Point3D

--- Point3D.cs
+++ Point3D.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 
 namespace Bev.SurfaceRasterData
 {
-    public class Point3D : IComparable
+    public class Point3D : IComparable<Point3D>
     {
         public Point3D(double x, double y, double z)
         {
@@ -18,19 +18,18 @@ namespace Bev.SurfaceRasterData
         public double Y { get; set; }
         public double Z { get; set; }
 
-        // this is a specialized ordering schema usefull for profile data only 
-        public int CompareTo(object obj)
-        {
-            Point3D objc = obj as Point3D;
-            if (X < objc.X) return -1;
-            if (X > objc.X) return 1;
-            return 0;
-        }
-
         public override string ToString()
         {
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
             return $"[Point3D - X:{X} Y:{Y} Z:{Z}]";
+        }
+
+        // this is a specialized ordering schema usefull for profile data only 
+        public int CompareTo(Point3D other)
+        {
+            if (X < other.X) return -1;
+            if (X > other.X) return 1;
+            return 0;
         }
     }
 }
